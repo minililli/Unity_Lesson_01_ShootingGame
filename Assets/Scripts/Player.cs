530d41b608fe97a7fa8d158e1fe1c65dc6956164
minililli <vanilla05284896@gmail.com>
2023-02-20 11:56:37 +0900

    Unity-VisualStudio utf matching
    Unity can read Korean in VisualStudio. txt -> editorconfig

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame updates
+    // 시작할 때 한번 실행되는 함수  
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
